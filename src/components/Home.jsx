b2a22b960554cde2b680de4a20dc104c45061d67
SureshThapa <sambridthapa6@gmail.com>
2025-12-17 14:39:56 +0545

    add properties page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,11 @@
 import React from "react";
 import Hero from "../components/hero";
-import Navbar from "../components/Navbar";
 import PropertyList from "../components/Propertylist";
 
 function Homepage() {
   return (
     <>
-      <Navbar />
+      
       <Hero />
       <PropertyList />
     </>
